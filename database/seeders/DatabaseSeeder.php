5aeee02de27965e30d19acb38d2d4edb1a972695
naser1945 <naser1945.2@gmail.com>
2024-10-27 01:16:46 +0300

    roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,12 +14,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Role::create(['name' => 'superadmin']);
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'cashier']);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $c = User::factory()->create([
+            'name' => 'cashier',
+            'email' => 'c@example.com',
         ]);
+        $c->assignRole('cashier');
+
+        $a = User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+        ]);
+        $a->assignRole('admin');
+
+        $sa = User::factory()->create([
+            'name' => 'superadmin',
+            'email' => 'superadmin@example.com'
+        ]);
+        $sa->assignRole('superadmin');
 
         $this->call([
             StudentSeeder::class,
