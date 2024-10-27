082f161c23fc04aa212b7a2094cc8636f8653586
naser1945 <naser1945.2@gmail.com>
2024-10-27 19:38:54 +0300

    users managment

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,14 +6,20 @@ namespace App\Models;
 use Filament\Models\Contracts\FilamentUser;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Models\Role;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory,
+        Notifiable,
+        HasRoles,
+        SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
