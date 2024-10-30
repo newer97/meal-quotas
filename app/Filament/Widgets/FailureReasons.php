e6b2dac2e8089541df4d9f321b9f91f04381c9d8
Nasser <naser1945.2@gmail.com>
2024-10-30 23:05:10 +0300

    change filure wiget to pie

--- app/Filament/Widgets/FailureReasons.php
+++ app/Filament/Widgets/FailureReasons.php
@@ -6,13 +6,40 @@ use App\Models\MealServe;
 use Filament\Tables;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
-use Filament\Widgets\TableWidget as BaseWidget;
+use Filament\Widgets\ChartWidget;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\DB;
 
-class FailureReasons extends BaseWidget
+class FailureReasons extends ChartWidget
 {
     protected static ?int $sort = 4;
+    protected static ?string $heading = 'Failure Reasons';
+
+    protected array|string|int $columnSpan = 1;
+
+    protected array $colors = [
+        "#FF6384", // Red
+        "#36A2EB", // Blue
+        "#FFCE56", // Yellow
+        "#4BC0C0", // Teal
+        "#9966FF", // Purple
+        "#FF9F40", // Orange
+        "#FFCD56", // Light Yellow
+        "#C9CBCF", // Light Gray
+        "#4D5360", // Dark Gray
+        "#FF6384", // Pinkish Red
+    ];
+
+    protected function getType(): string
+    {
+        return 'doughnut';
+    }
+
+    protected function getColors(int $count): array
+    {
+        return array_slice($this->colors, 0, $count);
+    }
+
 
     protected function getTableQuery(): Builder
     {
@@ -21,23 +48,18 @@ class FailureReasons extends BaseWidget
             ->whereNotNull('failure_reason')
             ->groupBy('failure_reason')->orderBy('total', 'desc');
     }
-    public function getTableRecordKey($record): string
-    {
-        return $record->failure_reason;
-    }
-    public function table(Table $table): Table
-    {
 
-        return $table
-            ->query($this->getTableQuery())
-            ->columns([
-                TextColumn::make('failure_reason')
-                    ->label('Reason')
-                    ->badge()
-                    ->color('danger'),
-                TextColumn::make('total')
-                    ->label('Count')
-                    ->numeric(),
-            ]);
+    protected function getData(): array
+    {
+        $data = $this->getTableQuery()->get();
+        return [
+            'labels' => $data->pluck('failure_reason'),
+            'datasets' => [
+                [
+                    'data' => $data->pluck('total'),
+                    'backgroundColor' => $this->getColors($data->count()),
+                ],
+            ],
+        ];
     }
 }
--- app/Filament/Widgets/LatestServes.php
+++ app/Filament/Widgets/LatestServes.php
@@ -11,7 +11,8 @@ use Filament\Widgets\TableWidget as BaseWidget;
 class LatestServes extends BaseWidget
 {
     protected static ?int $sort = 5;
-    protected int | string | array $columnSpan = 'full';
+    protected int | string | array $columnSpan = 1;
+
 
 
     public function table(Table $table): Table
