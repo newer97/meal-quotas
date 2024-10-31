f62d96a6cc2927986de591df90f2de99e483bb53
Nasser <naser1945.2@gmail.com>
2024-10-31 12:30:33 +0300

    fix policies

--- app/Policies/StudentPolicy.php
+++ app/Policies/StudentPolicy.php
@@ -2,26 +2,26 @@
 
 namespace App\Policies;
 
-use App\Models\Meal;
+use App\Models\MealServe;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class MealPolicy
+class StudentPolicy
 {
     /**
      * Determine whether the user can view any models.
      */
     public function viewAny(User $user): bool
     {
-        return true;
+        return $user->hasRole(['superadmin', 'admin']);
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Meal $meal): bool
+    public function view(User $user, MealServe $mealServe): bool
     {
-        return true;
+        return $user->hasRole(['superadmin', 'admin']);
     }
 
     /**
@@ -35,7 +35,7 @@ class MealPolicy
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Meal $meal): bool
+    public function update(User $user, MealServe $mealServe): bool
     {
         return $user->hasRole(['superadmin', 'admin']);
     }
@@ -43,7 +43,7 @@ class MealPolicy
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Meal $meal): bool
+    public function delete(User $user, MealServe $mealServe): bool
     {
         return $user->hasRole(['superadmin']);
     }
@@ -51,7 +51,7 @@ class MealPolicy
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Meal $meal): bool
+    public function restore(User $user, MealServe $mealServe): bool
     {
         return $user->hasRole(['superadmin']);
     }
@@ -59,7 +59,7 @@ class MealPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Meal $meal): bool
+    public function forceDelete(User $user, MealServe $mealServe): bool
     {
         return $user->hasRole(['superadmin']);
     }
